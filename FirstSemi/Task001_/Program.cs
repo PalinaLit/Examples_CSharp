941b7af9d2a27d33557d5f1131b5a1938f234383
Palina_Litvinava <palinalitvinava@icloud.com>
2022-12-15 22:12:48 +0300

    add FirstSemi Task 001 square of the number

---
+++ FirstSemi/Task001_/Program.cs
@@ -0,0 +1,9 @@
+﻿// Task001 Вывести квадрат числа
+
+Console.WriteLine("stars"); // начало
+
+int number = 13; // Определили целое число
+int result = number * number; // Вычисление кважрата числа
+Console.WriteLine (result); // Вывод результата
+
+Console.WriteLine("end"); // окончание
