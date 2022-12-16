b0af3e138e1206309424a4da08cfc83b5ebbe8dc
Palina_Litvinava <palinalitvinava@icloud.com>
2022-12-16 23:55:50 +0300

    add Feel intern Task 03 version 02

---
+++ MySelf_Tasks/Feel_Intern/Task03_version02/Program.cs
@@ -0,0 +1,50 @@
+﻿// 3. По заданному номеру дня недели вывести его название
+
+// start
+
+// int Monday = 1;
+// int Tuesday = 2;
+// int Wensday = 3;
+// int Thursday = 4;
+// int Friday = 5;
+// int Satarday = 6;
+// int Sunday = 7;
+
+Console.WriteLine("Введине число от 1 до 7 включительно");
+
+// Просим пользователя набрать одно из возможных чисел 
+// 1 2 3 4 5 6 7 
+
+string number = Console.ReadLine();
+// Вкладываем введенные пользователем данные в хранилище "day"
+
+int day = Convert.ToInt32(number);
+
+if(day == 1)
+{
+    Console.WriteLine("Monday");
+}
+if(day == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+if(day == 3)
+{
+    Console.WriteLine("Wensday");
+}
+if(day == 4)
+{
+    Console.WriteLine("Thursday");
+}
+if(day == 5)
+{
+    Console.WriteLine("Friday");
+}
+if(day == 6)
+{
+    Console.WriteLine("Satarday");
+}
+if(day == 7)
+{
+    Console.WriteLine("Sanday");
+}
