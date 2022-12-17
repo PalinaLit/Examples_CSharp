60c772be45eab9bf2c9a7c6926410d08fbd2d355
Palina_Litvinava <palinalitvinava@icloud.com>
2022-12-18 01:55:21 +0300

    add My self tastks Feel Inter task 04

---
+++ MySelf_Tasks/Feel_Intern/Task04/Program.cs
@@ -0,0 +1,23 @@
+﻿//  4. Найти максимальное из трех чисел
+
+// start
+
+int a = 3;
+int b = 4;
+int c = 5;
+
+int max = a;
+
+if(max < b)
+{
+    max = b;
+}
+if(max < c)
+{
+    max = c;
+
+}
+
+Console.WriteLine(max);
+
+
