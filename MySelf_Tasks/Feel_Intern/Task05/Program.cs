f88862d64fc7d8652debfb85d347498fa8ff647d
Palina_Litvinava <palinalitvinava@icloud.com>
2022-12-18 13:57:33 +0300

    add My self tastks Feel Inter task 05

---
+++ MySelf_Tasks/Feel_Intern/Task05/Program.cs
@@ -0,0 +1,12 @@
+﻿//  5. Написать программу вычисления значения функции y = f(a)
+
+// Например решим функция y = Sin(a)
+
+//start
+
+int a = 13;
+
+double result = Math.Sin(a);
+Console.WriteLine(result);
+
+
