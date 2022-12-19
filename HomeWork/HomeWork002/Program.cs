59a5fa59a25a7a85a101416c155d3317ff082a7f
Palina_Litvinava <palinalitvinava@icloud.com>
2022-12-19 16:35:24 +0300

    add empty strings betweem HW002 tasks

--- HomeWork/HomeWork002/Program.cs
+++ HomeWork/HomeWork002/Program.cs
@@ -1,4 +1,6 @@
 ﻿TaskHomeWork02.Task010();
+Console.WriteLine();
 TaskHomeWork02.Task013();
+Console.WriteLine();
 TaskHomeWork02.Task015();
 
---
+++ MySelf_Tasks/Feel_Intern/Task08/Program.cs
@@ -0,0 +1,22 @@
+﻿// 8. Показать четные числа от 1 до N
+
+// start
+
+Console.WriteLine("Введите любое число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("Четные числа от 1 до "+N);
+
+int number = 1;
+
+while(number < N)
+{
+    if(0 == number % 2)
+    {
+        Console.WriteLine(number);
+    }
+    number++;
+}
+
+// end
