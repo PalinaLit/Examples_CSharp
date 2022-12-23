4e251d8d967413dcbaec2164c01837f0ccb9784b
Palina_Litvinava <palinalitvinava@icloud.com>
2022-12-23 23:46:14 +0300

    Add Myself Task 10 code + save

--- MySelf_Tasks/Feel_Intern/Task10/Program.cs
+++ MySelf_Tasks/Feel_Intern/Task10/Program.cs
@@ -2,8 +2,5 @@
 
 Console.WriteLine("Input threedigit number");
 int userNumber = Convert.ToInt32(Console.ReadLine());
-
-
-
 string number = Convert.ToString(userNumber);
 Console.WriteLine("Your number's second figure -> "+number[1]);
