8dadd251f1ba7db1cdd9659a2506829339477a1f
Palina_Litvinava <palinalitvinava@icloud.com>
2022-12-23 23:45:34 +0300

    Add Myself Task 10 code

---
+++ MySelf_Tasks/Feel_Intern/Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// 10. Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Input threedigit number");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+
+
+string number = Convert.ToString(userNumber);
+Console.WriteLine("Your number's second figure -> "+number[1]);
