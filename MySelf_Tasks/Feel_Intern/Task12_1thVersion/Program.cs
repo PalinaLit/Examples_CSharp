ab181af4bcbf760c223d69554742e246175ddaa9
Palina_Litvinava <palinalitvinava@icloud.com>
2022-12-24 00:27:43 +0300

    Add Myself Task 12 1th version code

---
+++ MySelf_Tasks/Feel_Intern/Task12_1thVersion/Program.cs
@@ -0,0 +1,12 @@
+﻿// 12. Удалить вторую цифру трёхзначного числа
+
+// Console.WriteLine("Введите трехзначное число");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int N = new Random().Next(100, 1000);
+
+string number = Convert.ToString(new Random().Next(100, 1000));
+Console.WriteLine(number);
+Console.WriteLine("Удаляем вторую цифру трехначного числа -> "+number[0]+number[2]);
+
+
