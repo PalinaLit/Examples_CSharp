ea2d5f83ac566283f3b10ca236d7f00932dbc00c
Palina_Litvinava <palinalitvinava@icloud.com>
2022-12-24 15:25:15 +0300

    Add Myself Task 13

---
+++ MySelf_Tasks/Feel_Intern/Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.WriteLine("Задайте число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = new Random().Next(0, 1000);
+Console.WriteLine("Число от программы -> "+secondNumber);
+
+if(0 == secondNumber % firstNumber)
+{
+    Console.WriteLine("Число кратно заданному");
+}
+else
+{
+    int a = secondNumber % firstNumber;
+    Console.WriteLine("Остаток от деления числа программы на число заданное юзером -> "+a);
+}
+
