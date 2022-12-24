176402257a307ffc32248b2bb6761344c03315c7
Palina_Litvinava <palinalitvinava@icloud.com>
2022-12-24 14:37:30 +0300

    Add Myself Task 12 2th version code

---
+++ MySelf_Tasks/Feel_Intern/Task12_2ndVersion/Program.cs
@@ -0,0 +1,17 @@
+﻿// 12. Удалить вторую цифру трёхзначного числа
+
+
+string N = Convert.ToString(new Random().Next(100, 1000));
+Console.WriteLine("Трехзначное число -> "+N);
+Console.WriteLine("Удаляем вторую цифру трехзначного числа -> "+N.Remove(1, 1));
+
+// Метод String.Remove удаляет указанное количество знаков, 
+// начиная с указанного места в существующей строке. 
+// Этот метод подразумевает, что отсчет индекса начинается с нуля.
+// В следующем примере удаляется 10 символов из строки, 
+// начиная с позиции 5 отсчитываемого от нуля индекса строки.
+
+// string MyString = "Hello Beautiful World!";
+// Console.WriteLine(MyString.Remove(5,10));
+// The example displays the following output:
+//         Hello World!
