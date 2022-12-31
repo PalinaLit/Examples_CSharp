3fc880fbf1a20a9a8bf7515d34991e0a64c8da01
Palina_Litvinava <palinalitvinava@icloud.com>
2022-12-31 21:03:54 +0300

    add Home worf 05 last Task

--- HomeWork/HomeWork005/HW05_Library.cs
+++ HomeWork/HomeWork005/HW05_Library.cs
@@ -62,6 +62,11 @@ public static class HW05_Library
         Console.WriteLine("Количество четных чисел в массиве -> "+Convert.ToString(evenNumbersCount));
     }
 
+    /// <summary>
+    /// Метод подсчета суммы элементов массива
+    /// с несетными индексами 
+    /// </summary>
+    /// <param name="array">массив для подсчета</param>
     public static void ArrayOddIndexNumbersSum (int[] array)
     {
         int size = array.Length;
@@ -75,6 +80,65 @@ public static class HW05_Library
         }  
         Console.WriteLine("Сумма элементов с нечетным индексом -> "+Convert.ToString(sum));      
 
+    }
+
+    /// <summary>
+    /// Метод создания массива
+    /// для дальнейшего заполнения его ВЕЩЕСТВЕННЫМИ числами
+    /// </summary>
+    /// <param name="size">количество элементов массива</param>
+    /// <returns></returns>
+    public static double[] CreateRealNumredArray(int size)
+    {
+        return new double[size];
+    }
+
+    /// <summary>
+    /// Метод заполнения рандомными ВЕЩЕСТВЕННЫМИ значениями
+    /// и вывода массива
+    /// </summary>
+    public static void FillArrayWithRealNumbers(double[] array)
+    {
+        Random number = new Random();
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < array.Length; i++) 
+        {
+        array[i] = number.NextDouble() + new Random().Next(0, 100);
+        Console.WriteLine($"{Math.Round(array[i], 2)}");
+        }
+
+    }
+
+    /// <summary>
+    /// Метод определения и вывода разницы между
+    /// максимальным и минимальным 
+    /// элементом массива заполненного ВЕЩЕСТВЕННЫМИ числами
+    /// </summary>
+    /// <param name="array"></param>
+    public static void MinMaxArrayNumbers (double[] array)
+    {
+        int size = array.Length;
+        double max = array[0];
+        double min = array[0];
+        for(int i = 1; i < size; i++)
+        {
+            if(array[i] > max)
+            {
+                max = array[i];
+            }
+            else
+            {
+                if(array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+        }
+        Console.WriteLine("Максимальный элемнт массива ->"+$"{Math.Round(max, 2)}");
+        Console.WriteLine("Минимальный элемнт массива ->"+$"{Math.Round(min, 2)}");
+        Console.WriteLine();
+        double result = max - min;
+        Console.WriteLine("Разница между максимальным и минимальным элементом массива составила -> "+$"{Math.Round(result, 2)}");
     }
 
 }
--- HomeWork/HomeWork005/Program.cs
+++ HomeWork/HomeWork005/Program.cs
@@ -1,3 +1,5 @@
 ﻿HW05Tasks_Library.Task034();
 Console.WriteLine();
 HW05Tasks_Library.Task036();
+Console.WriteLine();
+HW05Tasks_Library.Task038();
