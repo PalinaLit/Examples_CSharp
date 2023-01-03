39c2128b84d22147ac00a70408eb9f6964da66e2
Palina_Litvinava <palinalitvinava@icloud.com>
2023-01-04 00:23:08 +0300

    add from myself tasks task 16 full complete

--- MySelf_Tasks/Task15_22/Tasks_Library.cs
+++ MySelf_Tasks/Task15_22/Tasks_Library.cs
@@ -12,6 +12,39 @@ public static class Tasks_Library
         Library.NumberMultiplicity(userNumber, 23);
     }
 
+    /// <summary>
+    /// 16. Дано число обозначающее день недели. 
+    /// Выяснить является номер дня недели выходным 
+    /// </summary>
+    public static void Task16()
+    {
+        int monday = UserNumberInput("What number will be for Monday?");
+        int tuesday = UserNumberInput("What number will be for Tuesday?");
+        int wensday = UserNumberInput("What number will be for Wensday?");
+        int thursday = UserNumberInput("What number will be for Thursday?");
+        int friday = UserNumberInput("What number will be for Friday?");
+        int saturday = UserNumberInput("What number will be for Saturday?");
+        int sunday = UserNumberInput("What number will be for Sunday?");
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        int userDayNumber = UserNumberInput("Input any day' number");
+
+        if (userDayNumber == saturday)
+        {
+            Console.WriteLine("This is weekend!");
+        }
+        else if (userDayNumber == sunday)
+        {
+            Console.WriteLine("This is weekend!");
+        }
+        else
+        {
+            Console.WriteLine("This is NOT weekend!");
+        }
+    }
+
 }
 
 
