eb97c88eb060d05535779ce1eee0d051410c7d12
Palina_Litvinava <palinalitvinava@icloud.com>
2023-01-03 23:40:17 +0300

    add task 14 writeline 3rd figure of number

--- MySelf_Tasks/Feel_Intern/Task14/Program.cs
+++ MySelf_Tasks/Feel_Intern/Task14/Program.cs
@@ -1,2 +1,20 @@
 ﻿// 14. Найти третью цифру числа или сообщить, что её нет
 
+// 1. Ввести число от юзера 
+Console.WriteLine("Введите любое целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// 2. Вывести третью цифру числа
+
+if (number / 100 < 1)
+{
+    Console.WriteLine("Третьей цифры в числе нет");
+    
+}
+// 3. Сообщить, что третьей цифры числа нет – если ее нет ;)
+else
+{
+    string newNumber = Convert.ToString(number);
+    Console.WriteLine("Третья цифра вашего числа -> "+newNumber[2]);
+}
+
