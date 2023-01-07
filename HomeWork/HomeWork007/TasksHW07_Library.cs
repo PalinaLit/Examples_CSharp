4d14dc83858046d686c34fa38ff153dccdf8ae50
Palina_Litvinava <palinalitvinava@icloud.com>
2023-01-07 15:35:15 +0300

    add home work 07 Task 52 Найдите среднее арифметическое элементов в каждом столбце FULL

--- HomeWork/HomeWork007/TasksHW07_Library.cs
+++ HomeWork/HomeWork007/TasksHW07_Library.cs
@@ -53,4 +53,33 @@ public static class TasksHW07_Library
 
         
     }
+
+    /// <summary>
+    /// Задача 52. Задайте двумерный массив из целых чисел. 
+    /// Найдите среднее арифметическое элементов в каждом столбце.
+    /// Например, задан массив:
+    /// 1 4 7 2
+    /// 5 9 2 3
+    /// 8 4 2 4
+    /// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+    /// </summary>
+    public static void Task052()
+    {
+        int a = UserInputIntNumber("Введите количество строк в двумерном массиве");
+        int b = UserInputIntNumber("Введите количество столбцов в двумерном массиве");
+
+        Console.WriteLine();
+
+        int[,] matrix = new int [a, b];
+        PrintArrayInt(matrix);
+
+        Console.WriteLine();
+
+        FillArrayInt(matrix);
+        PrintArrayInt(matrix);
+        Console.WriteLine();
+
+        HW07_Library.MatrixColummArithmeticMean(matrix);
+
+    }
 }
